9e670148a8fd7e6f2bf94398174bc7033271e78a
OtohaTanaka <otohaoppy@gmail.com>
2021-05-15 11:21:30 +0900

    Initial Commit

---
+++ TechMonster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechMonster
+//
+//  Created by 田中音羽 on 2021/05/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
